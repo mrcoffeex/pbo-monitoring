5d4ac63966fec544279214a4003589c642bae38f
Kent John Go <kjohn0319@gmail.com>
2025-10-17 14:39:47 +0800

    error fixed

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 namespace App\Filament\Resources\UserResource\Pages;
 
@@ -15,9 +15,9 @@ class CreateUser extends CreateRecord
 
     protected static string $resource = UserResource::class;
 
-    // protected function afterCreate(): void
-    // {
-    //     Mail::to($this->record->email)->send(new UserRegistrationMail($this->record));
-    // }
+    protected function afterCreate(): void
+    {
+        Mail::to($this->record->email)->send(new UserRegistrationMail($this->record));
+    }
 
 }
